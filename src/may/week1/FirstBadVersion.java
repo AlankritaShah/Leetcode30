cc5806e3799a3c3e83e537645fba7e8f032876e7
AlankritaShah <alankrita18.as@gmail.com>
2020-05-01 17:02:54 +0530

    may week1 problem1

--- src/may/week1/FirstBadVersion.java
+++ src/may/week1/FirstBadVersion.java
@@ -1,5 +1,61 @@
 package may.week1;
 
+/**
+ * 
+@author alankrita
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+Example:
+
+Given n = 5, and version = 4 is the first bad version.
+
+call isBadVersion(3) -> false
+call isBadVersion(5) -> true
+call isBadVersion(4) -> true
+
+Then 4 is the first bad version.
+ *
+ */
+
 public class FirstBadVersion {
+	
+	/* The isBadVersion API is defined in the parent class VersionControl.
+    boolean isBadVersion(int version); */
+
+	public int firstBadVersion(int n) {
+	      if(n == 0)
+	          return -1;
+	      
+	      return findBadVersion(0, n);  
+	  }
+	  
+	  private int findBadVersion(int start, int end){
+	      //System.out.println("start= " + start + " end= " + end);
+	      if(start == end)
+	          return isBadVersion(start)?start:-1;
+	      
+	      int mid = start + (end - start)/2;
+	      if(isBadVersion(mid)){
+	          //check left
+	          return findBadVersion(start, mid);
+	      }
+	      else{
+	          //check right
+	          return findBadVersion(mid+1, end);
+	      }
+	  }
+	  
+	  //This method is not required. Just for the sake of Eclipse, it is here.
+	  private boolean isBadVersion(int n) {
+		  return n>=4;
+	  }
+
+	public static void main(String[] args) {
+		  System.out.print((new FirstBadVersion()).firstBadVersion(5));
+	}
 
 }
